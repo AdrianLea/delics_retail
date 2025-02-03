96dd1a6154d1a39798abe3cd0f47ae38f13272b9
AdrianLea <1337floopix@gmail.com>
2025-02-03 18:06:37 +0000

    fixed instagram link

--- app/routes/($locale).products.$productHandle.jsx
+++ app/routes/($locale).products.$productHandle.jsx
@@ -106,13 +106,12 @@ export async function loader({params, request, context}) {
   const {productHandle} = params;
 
   const selectedOptions = getSelectedProductOptions(request);
-  if (selectedOptions.length > 0) {
-    const lastOption = selectedOptions[selectedOptions.length - 1];
-    lastOption.value = lastOption.value.split('?')[0];
-  }
-
   if (selectedOptions[0]?.name == '_kx') {
     selectedOptions.shift();
+  }
+
+  if (selectedOptions[selectedOptions.length - 1].name == 'fbclid') {
+    selectedOptions.pop();
   }
 
   const {shop, product} = await context.storefront.query(PRODUCT_QUERY, {
